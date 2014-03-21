f3d935d415891819d9f9dcbe7e0b8fd32a9fa2d5
Keri Clowes <kerilyn.clowes@gmail.com>
2014-03-21 09:29:51 -0600

    Refactored to use the Prime.prime method from the ruby standard library

--- lib/prime_number.rb
+++ lib/prime_number.rb
@@ -1,15 +1,7 @@
+require 'Prime'
+
 class PrimeNumber
   def is_prime?(number)
-    if number % 2 == 0 && number != 2
-      false
-    elsif number % 3 == 0 && number != 3
-      false
-    elsif number % 5 == 0 && number != 5
-      false
-    elsif number % 7 == 0 && number != 7
-      false
-    else
-      true
-    end
+    Prime.prime?(number)
   end
 end
