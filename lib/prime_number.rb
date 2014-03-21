5b9cd507596be66a56e707318c17d298c53c1785
Keri Clowes <kerilyn.clowes@gmail.com>
2014-03-21 09:25:05 -0600

    prime number detector detects if a number is non-prime

--- lib/prime_number.rb
+++ lib/prime_number.rb
@@ -1,15 +1,15 @@
 class PrimeNumber
   def is_prime?(number)
-    if number % 2 == 0 && number != 0
-      true
-    elsif number % 3 == 0 && number != 0
-      true
-    elsif number % 5 == 0 && number != 0
-      true
-    elsif number % 7 == 0 && number != 0
-      true
-    else
+    if number % 2 == 0 && number != 2
       false
+    elsif number % 3 == 0 && number != 3
+      false
+    elsif number % 5 == 0 && number != 5
+      false
+    elsif number % 7 == 0 && number != 7
+      false
+    else
+      true
     end
   end
 end
--- spec/prime_number_spec.rb
+++ spec/prime_number_spec.rb
@@ -2,10 +2,16 @@ require 'rspec/core'
 require './lib/prime_number'
 
 describe PrimeNumber do
-  it "detects whether or not 3 is a prime number" do
+  it "detects whether or not a number is a prime number" do
     pm = PrimeNumber.new
     actual = pm.is_prime?(3)
     expected = true
+    expect(actual).to eq(expected)
+  end
+  it "detects a non-prime number" do
+    pm = PrimeNumber.new
+    actual = pm.is_prime?(4)
+    expected = false
     expect(actual).to eq(expected)
   end
 
